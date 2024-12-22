46a23c44ab1c0637f33b5e7883468dd525d4a820
omar ahmed <omarahmedlove55@gmail.com>
2024-12-22 18:53:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.basic_img')
+let cont = document.querySelector('.container')
+function phones(x)
+{
+    img.src=x
+}
+function colors(y){
+    cont.style.background=y
+}
